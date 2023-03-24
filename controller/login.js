b320838a95d4c9f989e2554762d5a623c166c03e
Aks32216 <amishsingh568@gmail.com>
2023-03-24 08:00:46 +0530

    bcrypt added in login

--- controller/login.js
+++ controller/login.js
@@ -1,17 +1,56 @@
+const medilabDatabase=require('../models/db');
+const bcrypt=require('bcrypt');
+
 function loginGet(req,res){
     res.render('login');
 }
 
-function loginPost(req,res){
-    let {role,email,password}=req.body;
-    // verify from the backend about email and password
-    // once verified redirect to specific role page
-    if(role==="user"){
-        res.redirect('/user');
-    }else if(role==="doctor"){
-        res.redirect('/doctor')
-    }else{
-        res.redirect('/shopOwner')
+async function loginPost(req,res){
+    // let {role,email,password}=req.body;
+    // console.log(role);
+    // console.log(email);
+    // console.log(password);
+    // if(role==="users"){
+    //     res.redirect('/user');
+    // }else if(role==="doctor"){
+    //     res.redirect('/doctor')
+    // }else{
+    //     res.redirect('/shopOwner')
+    // }
+    try {
+        let {role,email,password}=req.body;
+        let sql="select * from "+role+" where _email=?;";
+        medilabDatabase.query(sql,[email],(err,result)=>{
+            if(err){
+                throw err;
+            }
+            console.log(result);
+            bcrypt.compare(password, result[0]._password, function(err, response) {
+                if (err){
+                    // for now logging the message
+                    console.log(err.message);
+                    return;
+                    // res.flash('error',err.message);
+                }
+                if (response) {
+                    // res.cookie('isLoggedIn',true);
+                    if(role==="users"){
+                        res.redirect('/user');
+                    }else if(role==="doctor"){
+                        res.redirect('/doctor')
+                    }else{
+                        res.redirect('/shopOwner')
+                    }
+                } else {
+                  // response is OutgoingMessage object that server response http request
+                  console.log("paswords do not match");
+                //   res.flash('error','Passwords do not match');
+                }
+              });
+        })
+    } catch (err) {
+        // res.flash('error',err.message);
+        console.log(err.message);
     }
 }
 
